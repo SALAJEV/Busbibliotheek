d219a57323c172336b2758149992dfd303df278f
Anzor Salajev <salajev@hotmail.com>
2026-07-08 16:00:08 +0200

    Update

--- site-config.js
+++ site-config.js
@@ -9,7 +9,7 @@ window.BB_SITE_CONFIG = {
   trackingUnavailableEnabled: 0,
   bannerEnabled: 0,
   bannerText: "Realtime-informatie kan tijdelijk vertraagd zijn.",
-  photoDelayEnabled: 1,
+  photoDelayEnabled: 0,
   photoDelayText: "Door omstandigheden worden foto's later geüpload dan normaal. Bedankt voor je begrip.",
   disabledTitle: "Busbibliotheek is tijdelijk niet beschikbaar",
   disabledBody: "De website is momenteel tijdelijk niet beschikbaar. Probeer het later opnieuw."
