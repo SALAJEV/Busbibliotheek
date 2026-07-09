0b71c0ff4cc28a74e8006a4d8482aa6f79fb830a
Anzor Salajev <salajev@hotmail.com>
2026-07-09 12:37:48 +0200

    Update

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = 'busbibliotheek-v109';
+const CACHE_NAME = 'busbibliotheek-v110';
 const APP_SHELL_CACHE_KEY = '/index.html';
 const CORE_ASSETS = [
   '/',
   APP_SHELL_CACHE_KEY,
-  '/app.js?v=20260708-4',
+  '/app.js?v=20260708-5',
   '/manifest.json',
-  '/style.css?v=20260708-4',
-  '/translations.js?v=20260708-4',
+  '/style.css?v=20260708-5',
+  '/translations.js?v=20260708-5',
   '/site-config.js?v=20260708-3',
   '/media/icons/logo.png',
   '/media/icons/navicon.png',
